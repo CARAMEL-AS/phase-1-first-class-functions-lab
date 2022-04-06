da941361bedad2ced9ec9e87e462854ab6baf695
Aftab Sidhu <Aftab180@gmail.com>
2022-04-06 16:22:13 -0400

    commit tree 33208bdf4b0f76be8979c112f8155e6a278b2976

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers (array) {
+    return (array.slice(0, 2));
+}
+
+function returnLastTwoDrivers (array) {
+    return (array.slice(-2))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function actualFareNultiplier(multiplier) {
+        return (integer * multiplier)
+    }
+}
+
+function fareDoubler(number) {
+    return(number * 2)
+}
+
+function fareTripler(number) {
+    return(number * 3)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, selectingDriver) {
+    return selectingDriver(arrayOfDrivers)
+}
